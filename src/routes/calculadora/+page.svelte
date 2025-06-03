de14cea2a6bceb4f8f49b5452247063c1c35ff9a
mavidiias <mavi.dias2008@gmail.com>
2025-06-03 08:28:03 -0400

    calculadora começo

---
+++ src/routes/calculadora/+page.svelte
@@ -0,0 +1,38 @@
+<div class="text-center mt-3">
+<input class="form-control">
+<table class="table table-borderless"> 
+    <tbody>
+        <tr>
+            <td><button type="button" class="btn bg-primary-subtle w-100">C</button></td>
+            <td><button type="button" class="btn btn-info w-100">&lpar;</button></td>
+            <td><button type="button" class="btn btn-info w-100">&rpar;</button></td>
+            <td><button type="button" class="btn btn-info w-100">/</button></td>
+        </tr>
+        <tr>
+            <td><button type="button" class="btn btn-info w-100">7</button></td>
+            <td><button type="button" class="btn btn-info w-100">8</button></td>
+            <td><button type="button" class="btn btn-info w-100">9</button></td>
+            <td><button type="button" class="btn btn-info w-100">x</button></td>
+        </tr>
+        <tr>
+            <td><button type="button" class="btn btn-info w-100">4</button></td>
+            <td><button type="button" class="btn btn-info w-100">5</button></td>
+            <td><button type="button" class="btn btn-info w-100">6</button></td>
+            <td><button type="button" class="btn btn-info w-100">-</button></td>
+        </tr>
+        <tr>
+            <td><button type="button" class="btn btn-info w-100">1</button></td>
+            <td><button type="button" class="btn btn-info w-100">2</button></td>
+            <td><button type="button" class="btn btn-info w-100">3</button></td>
+            <td rowspan="2" style="height: 0;">
+                <button type="button" class="btn btn-secondary w-100 w-100 h-100">+</button>
+            </td>
+        </tr>
+        <tr>
+            <td><button type="button" class="btn btn-info w-100">0</button></td>
+            <td><button type="button" class="btn btn-info w-100">.</button></td>
+            <td><button type="button" class="btn btn-info w-100">=</button></td>
+        </tr>
+    </tbody>
+</table>
+</div>
